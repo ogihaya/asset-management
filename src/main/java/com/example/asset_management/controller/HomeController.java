a53f59520fcec86f35d6c938088d965a27766198
OginoHayase <ogihaya1214@icloud.com>
2025-07-06 16:24:56 +0900

    今月の収入の入力を追加

--- src/main/java/com/example/asset_management/controller/HomeController.java
+++ src/main/java/com/example/asset_management/controller/HomeController.java
@@ -2,14 +2,11 @@ package com.example.asset_management.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
-    
-    @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("message", "Hello World");
+    @GetMapping("/")//@GetMappingは/にアクセスしたときに実行するという意味
+    public String home() {
         return "home";
     }
 }
